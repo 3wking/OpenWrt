de5f9965023be9c65e5c4a42b7c4f12e83a04cde
3wking <1094890624@qq.com>
2023-04-17 04:48:54 +0800

    更新 argon_img.sh

--- Shell/argon_img.sh
+++ Shell/argon_img.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
+RED_COLOR='\e[1;31m' #红色
+GREEN_COLOR='\e[1;32m' #绿色
+RES='\e[0m' #尾
+
+Install() {
 img="https://raw.iqiq.io/3wking/3wking.github.io/main/OpenWrt/IMG/Yamato_Kancolle.mp4"
-install() {
-echo "下载 img 图片......" 
-rm -f /www/luci-static/argon/background/* && curl --connect-timeout 30 -m 600 -#kLo /www/luci-static/argon/background/Yamato_Kancolle.mp4 "${img}"
+rm -f /www/luci-static/argon/background/*
+if [ $? -eq 0 ]; then
+	echo -e "${GREEN_COLOR}正在下载 $img ...${RES}"
+	curl --connect-timeout 30 -m 600 -#kLo /www/luci-static/argon/background/Yamato_Kancolle.mp4 "${img}"
+	if [ $? -eq 0 ]; then
+		echo -e "${GREEN_COLOR}安装<argon_img>完成${RES}\r\n"
+	fi	
+else
+	exit 1
+fi
 }
 (cd && {
-	echo "安装 argon_img 图片" && install && echo "安装 argon_img 图片完成"
+	echo -e "\r\n${GREEN_COLOR}安装<argon_img>图片${RES}\r\n" && Install
 })
