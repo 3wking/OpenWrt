203676898ffa337881db670b2270b1470d47b6ae
3wking <1094890624@qq.com>
2023-04-17 03:57:46 +0800

    更新 argon_img.sh

--- Shell/argon_img.sh
+++ Shell/argon_img.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 img="https://raw.iqiq.io/3wking/3wking.github.io/main/OpenWrt/IMG/Yamato_Kancolle.mp4"
 install() {
-echo "下载 argon_img 图片......" 
+echo "下载 img 图片......" 
 rm -f /www/luci-static/argon/background/* && curl --connect-timeout 30 -m 600 -#kLo /www/luci-static/argon/background/Yamato_Kancolle.mp4 "${img}"
 }
 (cd && {
