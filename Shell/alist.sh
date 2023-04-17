0bc4d6415e665247ab6ddd7bdcfc43ea588aab4b
3wking <1094890624@qq.com>
2023-04-17 09:53:04 +0800

    更新 alist.sh

--- Shell/alist.sh
+++ Shell/alist.sh
@@ -10,7 +10,7 @@ if [ -f /etc/openwrt_release ]; then
 	platform=$(echo $DISTRIB_ARCH)
 	framework=$(echo $DISTRIB_TARGET | awk -F '/' '{print $2}')
 else
-	echo -e "${RED_COLOR}错误： 未知的OpenWRT版本${RES}"
+	echo -e "\r\n${RED_COLOR}错误： 未知的OpenWRT版本${RES}\r\n"
 	exit 1
 fi
 #创建临时目录
@@ -29,17 +29,17 @@ Check() (
 	echo -e "\r\n${GREEN_COLOR}正在检查可用空间 ...${RES}"
 	ROOT_SPACE=$(df -m /usr | awk 'END{print $4}')
 	if [ $ROOT_SPACE -lt 20 ]; then
-		echo -e "\r\n${RED_COLOR}错误! 系统存储空间小于20MB.${RES}"
+		echo -e "\r\n${RED_COLOR}错误! 系统存储空间小于20MB.${RES}\r\n"
 		exit 1;
 	fi
-	echo -e "\r\n${GREEN_COLOR}检查OpenWrt架构 ...${RES}\r\n"
+	echo -e "\r\n${GREEN_COLOR}检查OpenWrt架构 ...${RES}"
 	prebuilt="aarch64_cortex-a53 aarch64_cortex-a72 aarch64_generic arm_arm1176jzf-s_vfp arm_arm926ej-s arm_cortex-a15_neon-vfpv4 arm_cortex-a5_vfpv4 arm_cortex-a7 arm_cortex-a7_neon-vfpv4 arm_cortex-a8_vfpv3 arm_cortex-a9 arm_cortex-a9_neon arm_cortex-a9_vfpv3-d16 arm_fa526 arm_mpcore arm_xscale i386_pentium-mmx i386_pentium4 mips64_octeonplus mips_24kc mips_4kec mips_mips32 mipsel_24kc mipsel_24kc_24kf mipsel_74kc mipsel_mips32 x86_64"
 	verif=$(expr match "$prebuilt" ".*\($platform\)")
 	if [[ ! $verif ]]; then
-		echo -e "${RED_COLOR}错误! \"$platform\" 平台当前不受支持.${RES}"
+		echo -e "\r\n${RED_COLOR}错误! \"$platform\" 平台当前不受支持.${RES}\r\n"
 		exit 1;
 	else
-		echo -e "${GREEN_COLOR}更新opkg来源 ...${RES}"
+		echo -e "\r\n${GREEN_COLOR}更新opkg来源 ...${RES}"
 		opkg update
 		#安装依赖
 		opkg install luci-compat
@@ -51,7 +51,7 @@ Download() (
 	# 获取软件包信息
 	curl -sk --connect-timeout 10 "https://api.github.com/repos/sbwml/luci-app-alist/releases" | grep "browser_download_url" > releases.txt
 	if [ $? -ne 0 ]; then
-		echo -e "${RED_COLOR}错误! 无法获取版本信息，请检查网络状态.${RES}"
+		echo -e "${RED_COLOR}无法获取版本信息，请检查网络状态.${RES}\r\n"
 		rm -rf $dir
 		exit 1
 	fi
@@ -64,21 +64,21 @@ Download() (
 	echo -e "${GREEN_COLOR}正在下载 $alist ...${RES}"
 	curl --connect-timeout 30 -m 600 -#kLO $mirror$alist
 	if [ $? -ne 0 ]; then
-		echo -e "\r\n${RED_COLOR}错误! 下载 $alist 失败.${RES}"
+		echo -e "${RED_COLOR}下载 $alist 失败.${RES}\r\n"
 		rm -rf $dir
 		exit 1
 	fi
 	echo -e "${GREEN_COLOR}正在下载 $luci_app ...${RES}"
 	curl --connect-timeout 30 -m 600 -#kLO $mirror$luci_app
 	if [ $? -ne 0 ]; then
-		echo -e "\r\n${RED_COLOR}错误! 下载 $luci_app 失败.${RES}"
+		echo -e "${RED_COLOR}下载 $luci_app 失败.${RES}\r\n"
 		rm -rf $dir
 		exit 1
 	fi
 	echo -e "${GREEN_COLOR}正在下载 $luci_i18n ...${RES}"
 	curl --connect-timeout 30 -m 600 -#kLO $mirror$luci_i18n
 	if [ $? -ne 0 ]; then
-		echo -e "\r\n${RED_COLOR}错误! 下载 $luci_i18n 失败.${RES}"
+		echo -e "${RED_COLOR}下载 $luci_i18n 失败.${RES}\r\n"
 		rm -rf $dir
 		exit 1
 	fi
@@ -91,7 +91,7 @@ Install() (
 	opkg install luci-app*.ipk
 	opkg install luci-i18n*.ipk
 	rm -rf $dir /tmp/luci-*
-	echo -e "\r\n${GREEN_COLOR}完成!${RES}"
+	echo -e "\r\n${GREEN_COLOR}完成!${RES}\r\n"
 )
 
 
